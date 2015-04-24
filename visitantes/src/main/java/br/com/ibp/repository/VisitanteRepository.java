430c630610a242b2bc8b2e114cd609d5839f2703
carlosrgomes <carlosrgomes@gmail.com>
2015-04-24 17:06:45 -0300

    adicionando paginacao

--- visitantes/src/main/java/br/com/ibp/repository/VisitanteRepository.java
+++ visitantes/src/main/java/br/com/ibp/repository/VisitanteRepository.java
@@ -3,13 +3,14 @@ package br.com.ibp.repository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import br.com.ibp.entity.Visitante;
 
 @RepositoryRestResource(path = "visitante")
-public interface VisitanteRepository extends CrudRepository<Visitante, Long> {
+public interface VisitanteRepository extends CrudRepository<Visitante, Long>, PagingAndSortingRepository<Visitante, Long> {
 	
 	List<Visitante> findByNome(@Param(value = "nome") String nome);
 	
