f83f1e2405f473a1ed994e42158b4b0b7ed79e98
carlosrgomes <carlosrgomes@gmail.com>
2015-04-24 17:02:37 -0300

    alterando metodos

--- visitantes/src/main/java/br/com/ibp/repository/VisitanteRepository.java
+++ visitantes/src/main/java/br/com/ibp/repository/VisitanteRepository.java
@@ -1,11 +1,18 @@
 package br.com.ibp.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import br.com.ibp.entity.Visitante;
 
 @RepositoryRestResource(path = "visitante")
 public interface VisitanteRepository extends CrudRepository<Visitante, Long> {
+	
+	List<Visitante> findByNome(@Param(value = "nome") String nome);
+	
+	List<Visitante> findByEmail(@Param(value = "email") String email);
 
 }
