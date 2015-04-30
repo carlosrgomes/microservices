43e0e310b69f2459dbf9f1f86bad4c8a255bcc8e
carlosrgomes <carlosrgomes@gmail.com>
2015-04-29 23:47:31 -0300

    alteracao

--- visitantes/src/main/java/br/com/ibp/configuration/App.java
+++ visitantes/src/main/java/br/com/ibp/configuration/App.java
@@ -3,11 +3,14 @@ package br.com.ibp.configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages="br.com.ibp.repository")
 @EntityScan(basePackages="br.com.ibp.entity")
+@EnableEurekaClient
+
 public class App {
 	
 	
